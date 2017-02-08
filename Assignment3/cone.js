7ba0b5d1d9067dfa620e7f1d762a761f85e50776
Aliel Reyes <alielreyes@gmail.com>
2017-02-07 18:17:32 -0800

    Update cone.js

--- Assignment3/cone.js
+++ Assignment3/cone.js
@@ -11,11 +11,17 @@ function init() {
         alert("Unable to setup WebGL");
         return;
     }
-
+    try
+    {
     gl.clearColor( 1.0, 0.0, 0.0, 1.0 );
     //cone = initShaders(cone, "Cone-vertex-shader", "Cone-fragment-shader"); //not working for me
     cone = new Cone(100);
     render();
+    }
+    catch(e)
+    {
+        alert(e.message);
+    }
 }
 
 function render() {
